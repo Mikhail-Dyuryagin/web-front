e3190993e063aa1a09b80efa9a5e9c8b09d7933b
denergym <01@transasia.ru>
2018-10-27 20:29:09 +0300

    app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function getSum() {
 
   console.log("Summing: " + a + " and " + b);
 
+
   $.get("https://denergym.devops.srwx.net/api/sum/?a=" + a + "&b=" + b, function( data ) {
     $("body")
       .append(" Status: " + data.Status)
