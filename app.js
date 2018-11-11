ab730f4225ce9698ab74bfae1f51edcdf9256d77
Lir <44003813+Mikhail-Dyuryagin@users.noreply.github.com>
2018-11-11 19:50:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function getSum() {
 
   console.log("Summing: " + a + " and " + b);
 
-  $.get(`https://${document.domain}/api/sum/?a=` + a + "&b=" + b, function( data ) {
+  $.get(`http://${document.domain}/api/sum/?a=` + a + "&b=" + b, function( data ) {
     $("body")
       .append(" Status: " + data.Status)
 	  .append(" Result: " + data.Result + "<br>")
@@ -18,7 +18,7 @@ function getDiff() {
 
   console.log("Difference between: " + a + " and " + b);
 
-  $.get(`https://${document.domain}/api/diff/?a=` + a + "&b=" + b, function( data ) {
+  $.get(`http://${document.domain}/api/diff/?a=` + a + "&b=" + b, function( data ) {
     $("body")
       .append(" Status: " + data.Status)
          .append(" Result: " + data.Result + "<br>")
